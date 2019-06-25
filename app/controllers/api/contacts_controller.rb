0ae318fb789c0ac157a468780d8bbd6674318369
Brian <riceb53@gmail.com>
2019-06-25 18:17:58 -0500

    add all_contacts route

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -3,4 +3,9 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.first
     render 'show.json.jb'
   end
+
+  def all
+    @contacts = Contact.all
+    render 'every_single_contact.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get '/contact' => 'contacts#show'
+    get '/all_contacts' => 'contacts#all'
   #   get "/photos" => "photos#index"
   end
 end
