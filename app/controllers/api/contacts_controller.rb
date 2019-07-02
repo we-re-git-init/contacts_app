9014111ff788953169d1b11d3995ac1a7dd5babe
Brian <riceb53@gmail.com>
2019-07-01 20:35:23 -0500

    add create action

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,3 +1,12 @@
 class Api::ContactsController < ApplicationController
-
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    @contact.save
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    # create. this will make new data in the db
+    post '/contacts' => 'contacts#create'
 
   #   get "/photos" => "photos#index"
   end
