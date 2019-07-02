624314b21965ba7f0d05260e2fe96e0addf5175f
Brian <riceb53@gmail.com>
2019-07-01 20:19:07 -0500

    remove non-RESTful routes/actions

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,11 +1,3 @@
 class Api::ContactsController < ApplicationController
-  def show
-    @contact = Contact.first
-    render 'show.json.jb'
-  end
 
-  def all
-    @contacts = Contact.all
-    render 'every_single_contact.json.jb'
-  end
 end
